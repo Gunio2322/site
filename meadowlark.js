0340d3cbea4f892d3315af11a26d1d81634afdae
Mariusz G <gunio2322@gmail.com>
2023-01-01 21:06:10 +0100

    formulaz logowania

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,24 @@
-
-
 const express = require('express')
 const expressHandlebars = require('express-handlebars').engine
-
+const bodyParser = require("body-parser")
 const app = express()
 const fortune = require('./lib/fortune')
+const { response } = require('express')
+
 // konfiguracja silnika widoków Handlebars
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
 }))
+// app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'  }))
 app.set('view engine', 'handlebars')
+// document.write(app.get("vive engine"))
 
 app.use(express.static(__dirname + '/public'))
 
@@ -17,9 +26,12 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() } )
-})
+app.get('/about', (req, res) => { res.render('about', { fortune: fortune.getFortune }
+
+  
+  )
+ })
+app.get('/section-test', (req, res) => { res.render('section-test')})
 
 // niestandardowa strona 404
 app.use((req, res) => {
@@ -34,7 +46,10 @@ app.use((err, req, res, next) => {
   res.render('500')
 })
 
-app.listen(port, () => console.log(
-  `Express został uruchomiony pod adresem http://localhost:${port}; ` +
-  `naciśnij Ctrl-C, aby zakończyć.`))
-  // console.log('This example is different!');tw
+app.listen(port, () =>
+  console.log(
+    `Server start http://localhost:${port}; ` +
+      `naciśnij Ctrl-C, aby zakończyć.`,
+  ),
+)
+// console.log('This example is different!');tw
